fba4961cd9c8bf0ced5216d3956c04f2c42f64b7
Mantas <syenergyy@gmail.com>
2019-10-18 22:19:25 +0300

    v2
    Now works properly, without increasing the bone speed

--- KK_DynamicBoneUpdateRate.cs
+++ KK_DynamicBoneUpdateRate.cs
@@ -1,38 +1,58 @@
 using Harmony;
 using BepInEx;
+
+using UnityEngine;
 using System.ComponentModel;
 
-[BepInPlugin(nameof(KK_DynamicBoneUpdateRate), nameof(KK_DynamicBoneUpdateRate), "1.0")]
-public class KK_DynamicBoneUpdateRate : BaseUnityPlugin {
+using JetBrains.Annotations;
 
-    #region Config properties
-        [DisplayName("DynamicBone Modifier Enable")]
-        [Description("Enable the plugin and set the DynamicBone update rate.")]
-        public static ConfigWrapper<bool> PluginEnabled { get; private set; }
-
-        [DisplayName("DynamicBone Update Rate")]
-        [Description("Stops boobs and skirt jiggle when above 60FPS. Change this value to your screen refresh rate and run with VSync for best results. Applies instantly.")]
-        [AcceptableValueRange(1, 240, false)]
-        public static ConfigWrapper<int> BoneUpdateRate { get; private set; }
-    #endregion
-
-    void Awake() {
+[BepInPlugin(nameof(KK_DynamicBoneUpdateRate), nameof(KK_DynamicBoneUpdateRate), "2.0")]
+public class KK_DynamicBoneUpdateRate : BaseUnityPlugin
+{
+    [DisplayName("DynamicBone Update Rate")]
+    [Description("DynamicBone Update Rate")]
+    [AcceptableValueRange(60, 144, false)]
+    private static ConfigWrapper<int> newUpdateRate { get; set; }
+    
+    private void Awake() {
         HarmonyInstance.Create(nameof(KK_DynamicBoneUpdateRate)).PatchAll(typeof(KK_DynamicBoneUpdateRate));
-
-        PluginEnabled = new ConfigWrapper<bool>("DynamicBone Modifier Enable", this, false);
-        BoneUpdateRate = new ConfigWrapper<int>("DynamicBone Update Rate", this, 60);
+        newUpdateRate = new ConfigWrapper<int>("DynamicBone-update-rate", this, 60);
     }
 
-    [HarmonyPrefix, HarmonyPatch(typeof(DynamicBone), "UpdateDynamicBones")]
-    public static void Pre_DynamicBone_UpdateDynamicBones(DynamicBone __instance) {
-        if (PluginEnabled.Value)
-            __instance.m_UpdateRate = BoneUpdateRate.Value;
+    [HarmonyPostfix, HarmonyPatch(typeof(DynamicBone))]
+    [UsedImplicitly]
+    public static void DynamicBone_Constructor_Patch(DynamicBone __instance)
+    {
+        __instance.m_UpdateRate = newUpdateRate.Value;
     }
-
-
-    [HarmonyPrefix, HarmonyPatch(typeof(DynamicBone_Ver02), "UpdateDynamicBones")]
-    public static void Pre_DynamicBone_Ver02_UpdateDynamicBones(DynamicBone_Ver02 __instance) {
-        if (PluginEnabled.Value)
-            __instance.UpdateRate = BoneUpdateRate.Value;
+    
+    [HarmonyPrefix, HarmonyPatch(typeof(DynamicBone), "Start")]
+    [UsedImplicitly]
+    public static void DynamicBone_Start_Patch(DynamicBone __instance)
+    {
+        __instance.m_UpdateRate = newUpdateRate.Value;
+        __instance.m_Gravity = new Vector3(
+            (__instance.m_Gravity.x * 60f) / newUpdateRate.Value, 
+            (__instance.m_Gravity.y * 60f) / newUpdateRate.Value, 
+            (__instance.m_Gravity.z * 60f) / newUpdateRate.Value
+        );
+    }
+    
+    [HarmonyPostfix, HarmonyPatch(typeof(DynamicBone_Ver02))]
+    [UsedImplicitly]
+    public static void DynamicBone_Ver02_Constructor_Patch(DynamicBone_Ver02 __instance)
+    {
+        __instance.UpdateRate = newUpdateRate.Value;
+    }
+    
+    [HarmonyPrefix, HarmonyPatch(typeof(DynamicBone_Ver02), "setGravity")]
+    [UsedImplicitly]
+    public static void DynamicBone_Ver02_setGravity_Patch(ref Vector3 _gravity)
+    {
+        _gravity = new Vector3(
+            (_gravity.x * 60f) / newUpdateRate.Value, 
+            (_gravity.y * 60f) / newUpdateRate.Value, 
+            (_gravity.z * 60f) / newUpdateRate.Value
+            );
     }
 }
